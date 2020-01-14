7b213b16b1ccaecd7d7cab8a5dfb00bb1800b662
Ben Ni <nizhangan123@Hotmail.com>
2020-01-14 02:28:48 -0500

    made some adjustment to the home page

--- src/home/index.js
+++ src/home/index.js
@@ -1,33 +1,56 @@
-import React, { useState } from "react"
-import { withRouter } from "react-router-dom"
-import { socket } from "../api"
-import "./home.css"
-import { Button } from 'reactstrap';
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import { socket } from '../api';
+import './home.css';
+import { Button, Container, Row, Col, Input } from 'reactstrap';
 
 const Home = ({ history }) => {
-	const [val, setVal] = useState("")
+    const [val, setVal] = useState('');
 
-	const onChange = e => {
-		setVal(e.target.value)
-	}
+    const onChange = e => {
+        setVal(e.target.value);
+    };
 
-	const globalClick = e => {
-		e.preventDefault()
-		socket.emit("join global", { room: "global", name: val })
-		history.push("/play")
-	}
+    const globalClick = e => {
+        e.preventDefault();
+        socket.emit('join global', { room: 'global', name: val });
+        history.push('/play');
+    };
 
-	return (
-		<div className="container">
-			<input className="name-input" type="text" value={val} onChange={e => onChange(e)} />
-			<Button color="primary" className="join-global" type="submit" onClick={e => globalClick(e)}>
-				Join
-			</Button>
-			<Button color="primary" className="join-private" type="submit">
-				Create Private Room
-			</Button>
-		</div>
-	)
-}
+    return (
+        <div className='container'>
+            <Input
+                className='name-input'
+                type='text'
+                value={val}
+                onChange={e => onChange(e)}
+                placeholder='Display Name'
+            />
+            <Container fluid={true}>
+                <row xs='2'>
+                    <Col>
+                        <Button
+                            color='primary'
+                            className='join-global'
+                            type='submit'
+                            onClick={e => globalClick(e)}
+                        >
+                            Play
+                        </Button>
+                    </Col>
+                    <Col>
+                        <Button
+                            color='info'
+                            className='join-private'
+                            type='submit'
+                        >
+                            Create Private Room
+                        </Button>
+                    </Col>
+                </row>
+            </Container>
+        </div>
+    );
+};
 
-export default withRouter(Home)
+export default withRouter(Home);
